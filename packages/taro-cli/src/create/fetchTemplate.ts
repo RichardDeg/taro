8bfffd62e4c0c08289e7f03416e606698afa979d
Richard D <richardd@RicharddeMacBook-Pro.local>
2024-12-19 11:49:35 +0800

    fix(taro-cli): 优化 fetchTemplate 逻辑，拆分 spinner cmd 控制台显示逻辑

--- packages/taro-cli/src/create/fetchTemplate.ts
+++ packages/taro-cli/src/create/fetchTemplate.ts
@@ -1,8 +1,9 @@
 import * as path from 'node:path'
 
-import { chalk, fs } from '@tarojs/helper'
-import * as AdmZip from 'adm-zip'
 import axios from 'axios'
+import { chalk, fs } from '@tarojs/helper'
+import type { FileStat } from '@tarojs/helper'
+import * as AdmZip from 'adm-zip'
 import * as download from 'download-git-repo'
 import * as ora from 'ora'
 
@@ -19,46 +20,34 @@ export interface ITemplates {
   compiler?: string[]
 }
 
-// TODO: 尝试将返回值的最外层 promise 和 then 函数 => 转为 async... await... 写法，是否可行
-// TODO: 看到这里了
 export default async function fetchTemplate (templateSource: string, templateRootPath: string, clone?: boolean): Promise<ITemplates[]> {
   const templateSourceType = getTemplateSourceType(templateSource)
   const isGitTemplate = templateSourceType === 'git'
   const isUrlTemplate = templateSourceType === 'url'
 
+  // 下载文件的缓存目录
   const templateDownloadDir = path.join(templateRootPath, TEMP_DOWNLOAD_FOLDER)
-  let name: string
+  if (fs.existsSync(templateDownloadDir)) await fs.remove(templateDownloadDir)
+  await fs.ensureDir(templateRootPath)
+  await fs.mkdir(templateDownloadDir)
 
-  // TODO: 尝试将最外层 promise 转为 async... await... 写法，是否可行
+  const spinner = ora(`正在从 ${templateSource} 拉取远程模板...`).start()
+
   // eslint-disable-next-line no-async-promise-executor
-  return new Promise<void>(async (resolve) => {
-    // 下载文件的缓存目录
-    if (fs.existsSync(templateDownloadDir)) await fs.remove(templateDownloadDir)
-    await fs.ensureDir(templateRootPath)
-    await fs.mkdir(templateDownloadDir)
-
-    const spinner = ora(`正在从 ${templateSource} 拉取远程模板...`).start()
+  return new Promise<{ error: string; templateSourceName: string } >(async (resolve) => {
     if (isGitTemplate) {
-      name = path.basename(templateSource)
-      download(templateSource, path.join(templateDownloadDir, name), { clone }, async error => {
-        if (error) {
-          spinner.color = 'red'
-          spinner.fail(chalk.red('拉取远程模板仓库失败！'))
-          console.log(error)
-          await fs.remove(templateDownloadDir)
-        } else {
-          spinner.color = 'green'
-          spinner.succeed(`${chalk.grey('拉取远程模板仓库成功！')}`)
-        }
-        return resolve()
+      const templateSourceName = path.basename(templateSource)
+      download(templateSource, path.join(templateDownloadDir, templateSourceName), { clone }, async error => {
+        if (error) await fs.remove(templateDownloadDir)
+        return resolve({ error, templateSourceName })
       })
     }
     if (isUrlTemplate) {
       try {
         // url 模板源，因为不知道来源名称，临时取名方便后续开发者从列表中选择
-        name = 'from-remote-url'
-        const zipPath = path.join(templateDownloadDir, `${name}.zip`)
-        const unzipDir = path.join(templateDownloadDir, name)
+        const originTemplateSourceName = 'from-remote-url'
+        const zipPath = path.join(templateDownloadDir, `${originTemplateSourceName}.zip`)
+        const unzipDir = path.join(templateDownloadDir, originTemplateSourceName)
         const response = await axios.get<fs.ReadStream>(templateSource, { responseType: 'stream' })
         const ws = fs.createWriteStream(zipPath)
         response.data.pipe(ws)
@@ -68,31 +57,32 @@ export default async function fetchTemplate (templateSource: string, templateRoo
           const zip = new AdmZip(zipPath)
           zip.extractAllTo(unzipDir, true)
 
-          const files = readDirWithFileTypes(unzipDir).filter(
-            file => !file.name.startsWith('.') && file.isDirectory && file.name !== '__MACOSX'
-          )
-          if (files.length !== 1) {
-            spinner.color = 'red'
-            spinner.fail(chalk.red(`拉取远程模板仓库失败！\n${new Error('远程模板源组织格式错误')}`))
-          } else {
-            spinner.color = 'green'
-            spinner.succeed(`${chalk.grey('拉取远程模板仓库成功！')}`)
-            name = path.join(name, files[0].name)
-          }
-          return resolve()
+          const files = readDirWithFileTypes(unzipDir).filter(filterFileByStatFn)
+          const isSuccessful = files.length === 1
+          const error = !isSuccessful ? `${new Error('远程模板源组织格式错误')}` : ''
+          const mergedTemplateSourceName = isSuccessful ? path.join(originTemplateSourceName, files[0].name) : originTemplateSourceName
+          return resolve({ error, templateSourceName: mergedTemplateSourceName })
         })
         ws.on('error', error => {
           throw error
         })
       } catch (error) {
-        spinner.color = 'red'
-        spinner.fail(chalk.red(`拉取远程模板仓库失败！\n${error}`))
         await fs.remove(templateDownloadDir)
-        return resolve()
+        return resolve(error)
       }
     }
-  }).then(async () => {
-    const templateDir = name ? path.join(templateDownloadDir, name) : ''
+  }).then(({ error, templateSourceName }) => {
+    if (error) {
+      spinner.color = 'red'
+      spinner.fail(chalk.red(`拉取远程模板仓库失败！\n${error}`))
+      console.log(error)
+    } else {
+      spinner.color = 'green'
+      spinner.succeed(`${chalk.grey('拉取远程模板仓库成功！')}`)
+    }
+    return templateSourceName
+  }).then(async (templateSourceName: string) => {
+    const templateDir = templateSourceName ? path.join(templateDownloadDir, templateSourceName) : ''
 
     /*************************** 默认空模板 **********************************/
     if (!fs.existsSync(templateDir)) return []
@@ -103,21 +93,21 @@ export default async function fetchTemplate (templateSource: string, templateRoo
 
     /*************************** 单模板 *************************************/
     if (hasSingleTemplate) {
-      await fs.move(templateDir, path.join(templateRootPath, name), { overwrite: true })
+      await fs.move(templateDir, path.join(templateRootPath, templateSourceName), { overwrite: true })
       await fs.remove(templateDownloadDir)
 
       const defaultTemplateDesc = isUrlTemplate ? templateSource : ''
-      const mergedTemplateItem = transformFile2Template(templateRootPath, name, defaultTemplateDesc)
+      const mergedTemplateItem = transformFile2Template(templateRootPath, templateSourceName, defaultTemplateDesc)
       return [mergedTemplateItem]
     }
 
     /*************************** 模板组 *************************************/
     const files = readDirWithFileTypes(templateDir)
-    const mergedFiles = files.filter(({ name: filename, isDirectory }) => isDirectory && !filename.startsWith('.') && filename !== '__MACOSX')
+    const mergedFiles = files.filter(filterFileByStatFn)
 
-    await Promise.all(mergedFiles.map(file => {
-      const src = path.join(templateDir, file.name)
-      const dest = path.join(templateRootPath, file.name)
+    await Promise.all(mergedFiles.map(({ name: filename }) => {
+      const src = path.join(templateDir, filename)
+      const dest = path.join(templateRootPath, filename)
       return fs.move(src, dest, { overwrite: true })
     }))
     await fs.remove(templateDownloadDir)
@@ -147,4 +137,8 @@ const transformFile2Template = (templateRootPath: string, filename: string, defa
   }
 
   return mergedTemplate
+}
+
+const filterFileByStatFn = ({ name, isDirectory }: FileStat): boolean => {
+  return isDirectory && !name.startsWith('.') && name !== '__MACOSX'
 }
