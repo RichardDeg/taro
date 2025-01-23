3a5fc2eab35f71e8e1654f7daf6835a8119018c6
Richard D <richardd@RicharddeMacBook-Pro.local>
2025-01-23 17:22:06 +0800

    fix(taro-cli): 了解 presets/commands/update.ts 指令注册与执行

--- packages/taro-cli/src/presets/commands/update.ts
+++ packages/taro-cli/src/presets/commands/update.ts
@@ -10,92 +10,25 @@ import { execCommand, getPkgItemByKey } from '../../util'
 
 import type { IPluginContext } from '@tarojs/service'
 
-// TODO: 看到这里了
 export default (ctx: IPluginContext) => {
   ctx.registerCommand({
     name: 'update',
     async fn ({ _, options }) {
-      const { npm } = options
       const [, updateType, version] = _ as [string, ('self' | 'project')?, string?]
       const { appPath, configPath } = ctx.paths
       const { chalk, fs, PROJECT_CONFIG, UPDATE_PACKAGE_LIST } = ctx.helper
-
-      // TODO: 看到这里了
-      async function getTargetVersion () {
-        let targetTaroVersion
-
-        if (version) {
-          targetTaroVersion = semver.clean(version)
-        } else {
-          const pkgName = getPkgItemByKey('name')
-
-          try {
-            targetTaroVersion = await getLatestVersion(pkgName, { version: 'latest' })
-          } catch (e) {
-            targetTaroVersion = await getLatestVersion(pkgName)
-          }
-        }
-        if (!semver.valid(targetTaroVersion)) {
-          console.log(chalk.red('命令错误:无效的 version ~'))
-          throw Error('无效的 version!')
-        }
-        return targetTaroVersion
-      }
-
-      function execUpdate (command: string, version: string, isSelf = false) {
-        const updateTarget = isSelf ? ' CLI ' : ' Taro 项目依赖'
-        const spinner = ora(`正在更新${updateTarget}到 v${version} ...`).start()
-        execCommand({
-          command,
-          successCallback (data) {
-            spinner.stop()
-            console.log(data.replace(/\n$/, ''))
-          },
-          failCallback (data) {
-            spinner.stop()
-            spinner.warn(data.replace(/\n$/, ''))
-          }
-        })
-      }
-
-      function askNpm () {
-        const packageChoices = [
-          {
-            name: 'yarn',
-            value: NpmType.Yarn
-          },
-          {
-            name: 'pnpm',
-            value: NpmType.Pnpm
-          },
-          {
-            name: 'npm',
-            value: NpmType.Npm
-          },
-          {
-            name: 'cnpm',
-            value: NpmType.Cnpm
-          }
-        ]
-        return [{
-          message: '请选择包管理工具',
-          name: 'npm',
-          type: 'list',
-          choices: packageChoices
-        }]
-      }
+      let npm = options.npm
 
       /** 更新全局的 Taro CLI */
       async function updateSelf () {
         const spinner = ora('正在获取最新版本信息...').start()
-        const targetTaroVersion = await getTargetVersion()
+        const taroVersion = await getSemanticVersion(version, chalk)
         spinner.stop()
-        console.log(chalk.green(`Taro 最新版本：${targetTaroVersion}\n`))
+        console.log(chalk.green(`Taro 最新版本：${taroVersion}\n`))
 
-        const npmPrompts = askNpm()
-        const answers = npm ? { npm } : await inquirer.prompt(npmPrompts)
-        const command = `${packagesManagement[answers.npm].globalCommand}@${targetTaroVersion}`
-        execUpdate(command, targetTaroVersion, true)
+        if(!npm) npm = await askNpm()
+        const command = `${packagesManagement[npm].globalCommand}@${taroVersion}`
+        execUpdate(command, taroVersion, true)
       }
 
       /** 更新当前项目中的 Taro 相关依赖 */
@@ -106,7 +39,7 @@ export default (ctx: IPluginContext) => {
         }
 
         const spinner = ora('正在获取最新版本信息...').start()
-        const version = await getTargetVersion()
+        const taroVersion = await getSemanticVersion(version, chalk)
         spinner.stop()
 
         const pkgPath = path.join(appPath, 'package.json')
@@ -115,32 +48,30 @@ export default (ctx: IPluginContext) => {
         // 更新 @tarojs/* 版本和 NervJS 版本
         Object.keys(pkgMap.dependencies || {}).forEach((key) => {
           if (UPDATE_PACKAGE_LIST.indexOf(key) !== -1) {
-            pkgMap.dependencies[key] = version
+            pkgMap.dependencies[key] = taroVersion
           }
         })
         Object.keys(pkgMap.devDependencies || {}).forEach((key) => {
           if (UPDATE_PACKAGE_LIST.indexOf(key) !== -1) {
-            pkgMap.devDependencies[key] = version
+            pkgMap.devDependencies[key] = taroVersion
           }
         })
 
         // 写入package.json
         try {
           await fs.writeJson(pkgPath, pkgMap, { spaces: '\t' })
-          console.log(chalk.green(`项目当前 Taro 版本：${oldVersion}，Taro 最新版本：${version}，更新项目 package.json 成功！`))
+          console.log(chalk.green(`项目当前 Taro 版本：${oldVersion}，Taro 最新版本：${taroVersion}，更新项目 package.json 成功！`))
           console.log()
         } catch (err) {
           console.error(err)
         }
 
-        const npmPrompts = askNpm()
-        const answers = npm ? { npm } : await inquirer.prompt(npmPrompts)
-        const command = packagesManagement[answers.npm].command
-        execUpdate(command, version)
+        if(!npm) npm = await askNpm()
+        const command = packagesManagement[npm].command
+        execUpdate(command, taroVersion)
       }
 
       if (updateType === 'self') return updateSelf()
-
       if (updateType === 'project') return updateProject()
 
       console.log(chalk.red('命令错误:'))
@@ -157,3 +88,71 @@ export default (ctx: IPluginContext) => {
     ]
   })
 }
+
+function execUpdate (command: string, version: string, isSelf = false) {
+  const updateTarget = isSelf ? ' CLI ' : ' Taro 项目依赖'
+  const spinner = ora(`正在更新${updateTarget}到 v${version} ...`).start()
+  execCommand({
+    command,
+    successCallback (data) {
+      spinner.stop()
+      console.log(data.replace(/\n$/, ''))
+    },
+    failCallback (data) {
+      spinner.stop()
+      spinner.warn(data.replace(/\n$/, ''))
+    }
+  })
+}
+
+async function getSemanticVersion (versionStr: string = '', chalk: any) {
+  let semanticVersion = ''
+  if (versionStr) {
+    semanticVersion = semver.clean(versionStr) || ''
+  } else {
+    const taroPkgName = getPkgItemByKey('name')
+    try {
+      semanticVersion = await getLatestVersion(taroPkgName, { version: 'latest' })
+    } catch (e) {
+      semanticVersion = await getLatestVersion(taroPkgName)
+    }
+  }
+
+  if (!semver.valid(semanticVersion)) {
+    console.log(chalk.red('命令错误:无效的 version ~'))
+    throw Error('无效的 version!')
+  }
+
+  return semanticVersion
+}
+
+async function askNpm (): Promise<NpmType> {
+  const packageChoices = [
+    {
+      name: 'yarn',
+      value: NpmType.Yarn
+    },
+    {
+      name: 'pnpm',
+      value: NpmType.Pnpm
+    },
+    {
+      name: 'npm',
+      value: NpmType.Npm
+    },
+    {
+      name: 'cnpm',
+      value: NpmType.Cnpm
+    }
+  ]
+
+  const npmPrompts = [{
+    message: '请选择包管理工具',
+    name: 'npm',
+    type: 'list',
+    choices: packageChoices
+  }]
+
+  const { npm } = await inquirer.prompt<{ npm: NpmType }>(npmPrompts)
+  return npm
+ }
