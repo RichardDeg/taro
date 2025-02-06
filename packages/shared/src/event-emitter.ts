c7f81d2f11866b56d77898583130df466b2064be
Richard D <richardd@RicharddeMacBook-Pro.local>
2025-02-06 22:04:23 +0800

    fix(taro-shared): 重命名 event-emitter 文件内的部分变量

--- packages/shared/src/event-emitter.ts
+++ packages/shared/src/event-emitter.ts
@@ -1,12 +1,17 @@
 type EventName = string | symbol
 // TODO: 待移除 any 类型，确定 EventCallbacks 与 node 的 ts 类型，还有可选参数问题
 // // TODO: 待确定，这样定义 ts 类型是否会引起循环引用问题
-// type EventCallbackNode = {
-//   next: EventCallbackNode
-//   tail?: EventCallbackNode
-//   callback?: CallbackFn
+// type EventNode = {
+//   next: EventNode | TailNode
+//   tail: TailNode
+//   callback: CallbackFn
 //   context?: any
 // }
+// type HeaderNode = {
+//   next: EventNode | TailNode
+//   tail: TailNode
+// }
+// type TailNode = {}
 type EventCallbacks = Record<EventName, Record<'next' | 'tail', Object>>
 type CallbackFn = (...args: any[]) => void
 
@@ -25,23 +30,31 @@ export class Events {
     for (const event of eventList) {
       if (!event) break
 
-      const tail = {}
+      const tailNode = {}
       if (this.callbacks[event]) {
-        this.callbacks[event].tail = tail
+        // TODO: 这个 if 分支功能待确定 ???
+        this.callbacks[event].tail = tailNode
       } else {
-        const node = { next: tail, callback, context }
-        this.callbacks[event] = { tail, next: node }
+        const eventNode = { next: tailNode, callback, context }
+        const headerNode = { tail: tailNode, next: eventNode }
+        this.callbacks[event] = headerNode
       }
     }
     return this
   }
 
+  /**
+   * 参考所有使用案例
+   * 1. off()                               - 清除所有事件
+   * 2. off(eventName, callback)            - 清除 eventName 的 callback 单个事件
+   * 3. off(eventName, undefined, context)  - 清除 eventName 的 context 上的所有事件
+   */
   // TODO: 看到这里了
   off (eventName?: EventName, callback?: CallbackFn, context?: any) {
     // 无注册事件，直接返回
     if (!this.callbacks) return this
 
-    // 清空所有注册事件
+    // 清除所有注册事件
     if (!eventName && !callback && !context) {
       delete this.callbacks
       return this
@@ -57,23 +70,21 @@ export class Events {
     for (const event of eventList) {
       if (!event) break
 
-      let node: any = this.callbacks[event]
-      // 清空指定注册事件, 删除 node 链表的全部节点
+      const headerNode: any = this.callbacks[event]
+      // 清除指定注册事件, 删除 node 链表的全部节点
       delete this.callbacks[event]
 
-      if (!!node && (!!callback || !!context)) {
-        const tail = node.tail
-        node = node.next
-
-        this.callbacks ||= {}
-        while (node !== tail) {
-          const cb = node.callback
-          const ctx = node.context
+      if (!!headerNode && (!!callback || !!context)) {
+        const tailNode = headerNode.tail
+        let eventNode = headerNode.next
+        while (eventNode !== tailNode) {
+          const cb = eventNode.callback
+          const ctx = eventNode.context
           // 重新注册事件, 重新创建 node 链表的节点（剔除 callback 或 context 的同属性节点）
           if ((callback && cb !== callback) || (context && ctx !== context)) {
             this.on(event, cb, ctx)
           }
-          node = node.next
+          eventNode = eventNode.next
         }
       }
     }
@@ -98,16 +109,16 @@ export class Events {
     for (const event of eventList) {
       if (!event) break
 
-      let node: any = this.callbacks[event]
-      if (!node) continue
+      const headerNode: any = this.callbacks[event]
+      if (!headerNode) continue
 
-      const tail = node.tail
-      node = node.next
-      while (node !== tail) {
-        const cb = node.callback
-        const ctx = node.context
+      const tailNode = headerNode.tail
+      let eventNode = headerNode.next
+      while (eventNode !== tailNode) {
+        const cb = eventNode.callback
+        const ctx = eventNode.context
         cb.apply(ctx || this, args)
-        node = node.next
+        eventNode = eventNode.next
       }
     }
     return this
