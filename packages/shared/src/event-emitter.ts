075b9dc79c21f2f73ed8618b98447208a3bc3d48
Richard D <richardd@RicharddeMacBook-Pro.local>
2025-02-09 22:07:55 +0800

    revert(taro-shared): 回退 event-emitter 文件内 off, trigger 方法的改动

--- packages/shared/src/event-emitter.ts
+++ packages/shared/src/event-emitter.ts
@@ -1,17 +1,4 @@
 type EventName = string | symbol
-// TODO: 待移除 any 类型，确定 EventCallbacks 与 node 的 ts 类型，还有可选参数问题
-// // TODO: 待确定，这样定义 ts 类型是否会引起循环引用问题
-// type EventNode = {
-//   next: EventNode | TailNode
-//   tail: TailNode
-//   callback: CallbackFn
-//   context?: any
-// }
-// type HeaderNode = {
-//   next: EventNode | TailNode
-//   tail: TailNode
-// }
-// type TailNode = {}
 type EventCallbacks = Record<EventName, Record<'next' | 'tail', Object>>
 type CallbackFn = (...args: any[]) => void
 
@@ -50,18 +37,9 @@ export class Events {
     return this
   }
 
-  /**
-   * 参考所有使用案例
-   * 1. off()                               - 清除所有事件
-   * 2. off(eventName, callback)            - 清除 eventName 的 callback 单个事件
-   * 3. off(eventName, undefined, context)  - 清除 eventName 的 context 上的所有事件
-   */
-  // TODO: 看到这里了
   off (eventName?: EventName, callback?: CallbackFn, context?: any) {
-    // 无注册事件，直接返回
     if (!this.callbacks) return this
 
-    // 清除所有注册事件
     if (!eventName && !callback && !context) {
       delete this.callbacks
       return this
@@ -73,25 +51,25 @@ export class Events {
         ? eventName.split(Events.eventSplitter)
         : Object.keys(this.callbacks)
 
-    // TODO: 待重构，以双节点指针的形式，改写 node 链表上的节点
     for (const event of eventList) {
       if (!event) break
 
-      const headNode: any = this.callbacks[event]
+      let node: any = this.callbacks[event]
       // 清除指定注册事件, 删除 node 链表的全部节点
       delete this.callbacks[event]
+      if (!node || (!callback && !context)) continue
 
-      if (!!headNode && (!!callback || !!context)) {
-        const tailNode = headNode.tail
-        let eventNode = headNode.next
-        while (eventNode !== tailNode) {
-          const cb = eventNode.callback
-          const ctx = eventNode.context
+      if (!!node && (!!callback || !!context)) {
+        const tail = node.tail
+        node = node.next
+        while (node !== tail) {
+          const cb = node.callback
+          const ctx = node.context
           // 重新注册事件, 重新创建 node 链表的节点（剔除 callback 或 context 的同属性节点）
           if ((callback && cb !== callback) || (context && ctx !== context)) {
             this.on(event, cb, ctx)
           }
-          eventNode = eventNode.next
+          node = node.next
         }
       }
     }
@@ -103,9 +81,7 @@ export class Events {
       callback.apply(this, wrapperArgs)
       this.off(eventName, wrapper, context)
     }
-
     this.on(eventName, wrapper, context)
-
     return this
   }
 
@@ -116,16 +92,16 @@ export class Events {
     for (const event of eventList) {
       if (!event) break
 
-      const headNode: any = this.callbacks[event]
-      if (!headNode) continue
+      let node: any = this.callbacks[event]
+      if (!node) continue
 
-      const tailNode = headNode.tail
-      let eventNode = headNode.next
-      while (eventNode !== tailNode) {
-        const cb = eventNode.callback
-        const ctx = eventNode.context
+      const tail = node.tail
+      node = node.next
+      while (node !== tail) {
+        const cb = node.callback
+        const ctx = node.context
         cb.apply(ctx || this, args)
-        eventNode = eventNode.next
+        node = node.next
       }
     }
     return this
